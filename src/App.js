0606a0e96ac23cff8fb75b65e2122c7bb0deacba
hardik <hardikganatra20@gmail.com>
2021-11-28 16:13:48 +0530

    TicTacToe:  Logic Done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
 import "./App.css";
+import React from "react";
 
-const squares = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-const renderSquare = (i) => {
-  return <button className="square">{squares[i]}</button>;
-};
 const Board = () => {
+  const [squares, setSquares] = React.useState(() => Array(9).fill(null));
+  const calcNextValue = (squares) => {
+    return squares.filter(Boolean).length % 2 === 0 ? "X" : "O";
+  };
+  const calcWinner = (squares) => {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < winningCombinations.length; i++) {
+      const [a, b, c] = winningCombinations[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[b] === squares[c]
+      ) {
+        return squares[c];
+      }
+    }
+    return null;
+  };
+  const calcStatus = (winner, squares, nextValue) => {
+    if (winner) {
+      return `Winner is ${winner}`;
+    } else if (squares.every(Boolean)) {
+      return "Its a Draw";
+    } else {
+      return `Next Player ${nextValue}`;
+    }
+  };
+  const nextValue = calcNextValue(squares);
+  const winner = calcWinner(squares);
+  const status = calcStatus(winner, squares, nextValue);
+
+  const selectSquare = (squareIndex) => {
+    if (winner) {
+      return;
+    } else {
+      const squaresCopy = [...squares];
+      squaresCopy[squareIndex] = nextValue;
+      setSquares(squaresCopy);
+    }
+  };
+
+  const renderSquare = (i) => {
+    return (
+      <button className="square" onClick={() => selectSquare(i)}>
+        {squares[i]}
+      </button>
+    );
+  };
   return (
     <div>
-      <div className="status">STATUS</div>
+      <div className="status">{status}</div>
       <div className="container">
         <div className="board-row">
           {renderSquare(0)}
@@ -26,7 +79,14 @@ const Board = () => {
           {renderSquare(8)}
         </div>
       </div>
-      <button className="restart">Restart</button>
+      <button
+        className="restart"
+        onClick={() => {
+          setSquares(Array(9).fill(null));
+        }}
+      >
+        Restart
+      </button>
     </div>
   );
 };
@@ -44,11 +104,7 @@ const Game = () => {
 function App() {
   return (
     <>
-      <div class="parent">
-        <div class="center">
-          <Game />
-        </div>
-      </div>
+      <Game />
     </>
   );
 }

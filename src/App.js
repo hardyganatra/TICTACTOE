73a2a2f0e6f5e48e0d1b8c9e8e699411261b074a
hardik <hardikganatra20@gmail.com>
2021-11-28 15:07:01 +0530

    TicTacToe:  Develop Board html and css using flexbox

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const squares = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+const renderSquare = (i) => {
+  return <button className="square">{squares[i]}</button>;
+};
+const Board = () => {
+  return (
+    <div>
+      <div className="status">STATUS</div>
+      <div className="container">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      <button className="restart">Restart</button>
+    </div>
+  );
+};
+
+const Game = () => {
+  return (
+    <div>
+      <div className="game">
+        <Board />
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="parent">
+        <div class="center">
+          <Game />
+        </div>
+      </div>
+    </>
   );
 }
 

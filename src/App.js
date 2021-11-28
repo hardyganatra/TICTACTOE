2b58df2a9928c7b236ce668fdf482216b67e0317
hardik <hardikganatra20@gmail.com>
2021-11-28 17:28:19 +0530

    TicTacToe:  Adding Particles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
+import Particles from "react-tsparticles";
 
-const Board = () => {
+const Board = ({ setWinner }) => {
   const [squares, setSquares] = React.useState(() => Array(9).fill(null));
   const calcNextValue = (squares) => {
     return squares.filter(Boolean).length % 2 === 0 ? "X" : "O";
   };
+
   const calcWinner = (squares) => {
     const winningCombinations = [
       [0, 1, 2],
@@ -24,8 +26,10 @@ const Board = () => {
         squares[a] === squares[b] &&
         squares[b] === squares[c]
       ) {
+        setWinner(true);
         return squares[c];
       }
+      setWinner(false);
     }
     return null;
   };
@@ -91,20 +95,180 @@ const Board = () => {
   );
 };
 
-const Game = () => {
+const Game = ({ setWinner }) => {
   return (
     <div>
       <div className="game">
-        <Board />
+        <Board setWinner={setWinner} />
       </div>
     </div>
   );
 };
 
 function App() {
+  const [winner, setWinner] = React.useState(null);
+
   return (
     <>
-      <Game />
+      {winner && (
+        <Particles
+          className="particles"
+          id="tsparticles"
+          options={{
+            fullScreen: {
+              enable: true,
+            },
+            detectRetina: true,
+            background: {
+              color: "rgba(158, 116, 175, 0.692)",
+            },
+            fpsLimit: 60,
+            emitters: {
+              direction: "top",
+              life: {
+                count: 0,
+                duration: 0.1,
+                delay: 0.1,
+              },
+              rate: {
+                delay: 0.5,
+                quantity: 1,
+              },
+              size: {
+                width: 100,
+                height: 0,
+              },
+              position: {
+                y: 100,
+                x: 50,
+              },
+            },
+            particles: {
+              number: {
+                value: 0,
+              },
+              destroy: {
+                mode: "split",
+                split: {
+                  count: 1,
+                  factor: { value: 1 / 3 },
+                  rate: {
+                    value: 100,
+                  },
+                  particles: {
+                    stroke: {
+                      color: {
+                        value: [
+                          "#5bc0eb",
+                          "#fde74c",
+                          "#9bc53d",
+                          "#e55934",
+                          "#fa7921",
+                        ],
+                      },
+                      width: 1,
+                    },
+                    number: {
+                      value: 0,
+                    },
+                    collisions: {
+                      enable: false,
+                    },
+                    opacity: {
+                      value: 1,
+                      animation: {
+                        enable: true,
+                        speed: 0.6,
+                        minimumValue: 0.1,
+                        sync: false,
+                        startValue: "max",
+                        destroy: "min",
+                      },
+                    },
+                    shape: {
+                      type: "circle",
+                    },
+                    size: {
+                      value: 1,
+                      animation: {
+                        enable: false,
+                      },
+                    },
+                    life: {
+                      count: 1,
+                      duration: {
+                        value: {
+                          min: 1,
+                          max: 2,
+                        },
+                      },
+                    },
+                    move: {
+                      enable: true,
+                      gravity: {
+                        enable: false,
+                      },
+                      speed: 2,
+                      direction: "none",
+                      random: true,
+                      straight: false,
+                      outMode: "destroy",
+                    },
+                  },
+                },
+              },
+              life: {
+                count: 1,
+              },
+              shape: {
+                type: "line",
+              },
+              size: {
+                value: { min: 0.1, max: 50 },
+                animation: {
+                  enable: true,
+                  sync: true,
+                  speed: 150,
+                  startValue: "max",
+                  destroy: "min",
+                },
+              },
+              stroke: {
+                color: {
+                  value: "#ffffff",
+                },
+                width: 1,
+              },
+              rotate: {
+                path: true,
+              },
+              move: {
+                enable: true,
+                gravity: {
+                  acceleration: 15,
+                  enable: true,
+                  inverse: true,
+                  maxSpeed: 100,
+                },
+                speed: { min: 10, max: 20 },
+                outModes: {
+                  default: "destroy",
+                  top: "none",
+                },
+                trail: {
+                  enable: true,
+                  length: 10,
+                },
+              },
+            },
+          }}
+        />
+      )}
+
+      <div className="game-container">
+        <div className="title">Awesome Tic Tac Toe</div>
+        <Game setWinner={setWinner} />
+      </div>
     </>
   );
 }
